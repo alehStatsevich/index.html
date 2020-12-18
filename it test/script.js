a502ba47ebd174c291775476083617ee8d1ab6be
oleg-s-by <70647648+oleg-s-by@users.noreply.github.com>
2020-12-18 21:38:33 +0300

    test social security

---
+++ it test/script.js
@@ -0,0 +1,85 @@
+//задание 1.2.3
+//let search = document.getElementById("search");
+//let butt = document.getElementById("butt");
+
+//butt.addEventListener("click", () => {
+//	alert(search.value)
+//});
+//задание 4
+/*function buttonClick() {
+	var input = document.getElementById('input');
+	input.value = "Yandex круче.Но зтоне точно";
+}*/
+//задача 5
+/*let array = [
+    {name:"nik", age:"25"},
+    {name:"majk", age:"30"},
+    {name:"ola",age:"20"},
+
+];
+function buttonClick() {
+	var input = document.getElementById('input');
+alert(array[0]["name"]);
+}*/
+//задача 6
+/*let sum =(a=2,b=3);
+function superSum () {
+    sum = a+b;
+    let result = sum;
+    alert(result);
+}
+superSum();*/
+//задача 7
+/*let arr = [1,2,3,-4];
+for (let i in arr) {
+    if (arr[i] === Math.max && arr[i] === Math.min);
+console.log (arr[i]);*/
+
+/*let array = [1,2,3,-4];
+let max = Max(array);
+let min = Min(array);
+alert(max);
+alert(min);
+ 
+// получение максимального элемента массива
+function Max(array){
+    let max = array[0]; // берем первый элемент массива
+    for (let i = 0; i < array.length; i++) { // переберем весь массив
+        // если элемент больше, чем в переменной, то присваиваем его значение переменной
+        if (max < array[i]) max = array[i]; 
+    }
+    // возвращаем максимальное значение
+    return max;
+}
+ 
+// получение минимального элемента массива
+function Min(array){
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
+}*/
+//задача8
+//let a = 5;
+//let b = 6;
+//[a, b] = [b, a];
+//console.log(a,b);
+//задача 9
+/*let numArray =[1,2,3,4];
+let max = findMax(numArray);
+alert(max);
+function findMax (numArray) {
+    return Math.max.apply(null, numArray);
+  }*/
+//задача 10
+  /*function setInterval() {
+    alert('Привет');
+  }
+   setTimeout(setInterval, 3000);*/
+
+
+
+
+
+
